81f3a9315987dfefc5327f917a861095b7fa6e67
Douglas <douglasitba2016@gmail.com>
2023-09-04 19:45:19 -0300

    first commit

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const dbConfig ='mongodb+srv://doug123:DouglasJoseph1234@cluster0.zu3utl5.mongodb.net/annotations?retryWrites=true&w=majority'
+const dbConfig =''
 
 const connection = mongoose.connect(dbConfig, {
 useNewUrlParser: true,
